2d364de50ae502b456449d5177caee6f7f461ee6
Corin Ziyadeh <corin@boxboat.com>
2021-01-28 15:52:30 -0500

    Corrected mod path

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import wh "github.com/corinz/go-webhook-receiver/tree/dev"
+import wh "github.com/corinz/go-webhook-receiver"
 
 func main() {
 	wh.ExecuteThisWhen("slack-notify.sh", "")
